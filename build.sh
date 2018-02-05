79e701edd42a549de330d6f30742eb1bffe89c89
Iiro Rahkonen <DeadBurger@users.noreply.github.com>
2018-02-05 20:37:34 +0200

    Fixing build

--- build.sh
+++ build.sh
@@ -9,12 +9,10 @@ if [ -d $artifactsFolder ]; then
   rm -R $artifactsFolder
 fi
 
-dotnet restore
+dotnet build
 dotnet build -c Release
 dotnet test HooksNet.Console.Tests -c Release
 dotnet test HooksNet.Tests -c Release
 
 revision=${TRAVIS_JOB_ID:=1}  
-revision=$(printf "%04d" $revision) 
-
-dotnet pack HooksNet /p:NuspecFile="..\HooksNet.nuspec" /p:NuspecProperties="version=0.1.$revision" -c Release -o ./artifacts
+revision=$(printf "%04d" $revision)
