31c68f4bbd0713920d3c39ec5ab51bc71e0f031a
Iiro Rahkonen <DeadBurger@users.noreply.github.com>
2018-02-11 13:53:16 +0200

    Add nuget publish

--- build.sh
+++ build.sh
@@ -17,3 +17,4 @@ revision=${TRAVIS_JOB_ID:=1}
 revision=$(printf "%04d" $revision)
 
 dotnet pack HooksNet /p:NuspecFile=../HooksNet.nuspec -c Release -o ./artifacts --version-suffix=$revision  
+dotnet nuget push ./artifacts/*.nupkg -k $NugetApikey
